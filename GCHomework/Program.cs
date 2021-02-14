f854cfeae953c121bae37cd32859f3320323d5f7
AdreN167 <zz11926557@gmail.com>
2021-02-14 15:50:09 +0600

    Домашенне задание

---
+++ GCHomework/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace GCHomework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //-----------------------1-----------------------
+            var consoleType = typeof(Console);
+            foreach (var method in consoleType.GetMethods())
+            {
+                Console.Write($"{method.Name}");
+
+                Console.Write("(");
+                foreach (var parameter in method.GetParameters())
+                {
+                    Console.Write($" {parameter.ParameterType}->{parameter.Name} ");
+                }
+                Console.WriteLine(")");
+            }
+            Console.WriteLine();
+
+            //-----------------------2-----------------------
+            var student = new Student()
+            {
+                Id = 1,
+                Name = "Andrey",
+                Group = "SEP-201",
+                AverageMark = 11.2
+            };
+
+            foreach (var property in typeof(Student).GetProperties())
+            {
+                Console.WriteLine($"{property.Name} = {property.GetValue(student)}");
+            }
+        }
+    }
+}
---
+++ GCHomework/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace GCHomework
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public double AverageMark { get; set; }
+    }
+}
